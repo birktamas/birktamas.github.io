22e9a9f1d75114a3778009f5808f51a4b85f1384
birktamas <birktamas@citromail.hu>
2020-07-11 22:20:48 +0200

    Init

--- js/rendeles.js
+++ js/rendeles.js
@@ -1,16 +1,34 @@
-function fizetendőKiszámolás() {
-    let választottHamburger = document.querySelector("name=['hamburger']");
-    let hamburgerÁr = parseInt(választottHamburger.value);
+function calcAmount() {
+    let klasszikÁr = 1000;
+    let sajtburgerÁr = 1100;
+    let háromsajtosÁr = 1400;
+    let zöldségesÁr = 1200;
+    let baconösÁr = 1200;
+    let óriásÁr = 1700;
 
-         
-    mutasdHamburgerÁr(hamburgerÁr, feltétÁr)
-}
-
-function mutasdHamburgerÁr(hamburgerÁr, feltétár) {
-    let mutasdAzÖsszeget = document.querySelector("span.MutasdAzÖsszeget")
+    let amountInputKlasszik = document.querySelector("input[name='amount-input-klasszik']");
+    let amountNumberKlasszik = parseInt(amountInputKlasszik.value);
     
-         
-        mutasdAzÖsszeget.innerHTML = hamburgerÁr;
-    }
+    let amountInputSajtburger = document.querySelector("input[name='amount-input-sajtburger']");
+    let amountNumberSajtburger = parseInt(amountInputSajtburger.value);
+
+    let amountInput3sajtos = document.querySelector("input[name='amount-input-3sajtos']");
+    let amountNumber3sajtos = parseInt(amountInput3sajtos.value);
+
+    let amountInputZöldséges = document.querySelector("input[name='amount-input-zöldséges']");
+    let amountNumberZöldséges = parseInt(amountInputZöldséges.value);
+
+    let amountInputBaconös = document.querySelector("input[name='amount-input-baconös']");
+    let amountNumberBaconös = parseInt(amountInputBaconös.value);
+
+    let amountInputÓriás = document.querySelector("input[name='amount-input-óriás']");
+    let amountNumberÓriás = parseInt(amountInputÓriás.value);
+
+    let amount = parseInt(amountNumberKlasszik) * klasszikÁr + (sajtburgerÁr * parseInt(amountNumberSajtburger)) + (háromsajtosÁr * parseInt(amountNumber3sajtos)) + (zöldségesÁr * parseInt(amountNumberZöldséges)) + (baconösÁr * parseInt(amountNumberBaconös)) + (óriásÁr * parseInt(amountNumberÓriás));
+
+    let showAmount = document.querySelector("span.show-amount");
+
+    showAmount.innerHTML = amount;
+
 
 }
